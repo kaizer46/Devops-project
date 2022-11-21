716b05dd9664a92896849bd0aa621cac2ad43178
kaizer46 <taykaize46@gmail.com>
2022-11-21 13:11:34 +0800

    first commit
    Signed-off-by: kaizer46 <taykaize46@gmail.com>

---
+++ src/main/java/com/dvops/maven/eclipse/HelloServlet.java
@@ -0,0 +1,46 @@
+package com.dvops.maven.eclipse;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String yourName = request.getParameter("yourName");
+		PrintWriter writer = response.getWriter();
+		writer.println("<h1>Hello " + yourName + "</h1>");
+		writer.close();
+		doGet(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doGet(request, response);
+	}
+
+}
